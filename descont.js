024f7aeeb1a99bfdefc785bea54b1067aea8df88
Aderaldo <juniorcosta2611@gmail.com>
2025-06-29 12:29:36 -0300

    script descont

--- descont.js
+++ descont.js
@@ -4,11 +4,41 @@ document.addEventListener('DOMContentLoaded', function () {
   const msgDesconto = divDesconto.children[2];
   const precoDesconto = divDesconto.lastElementChild;
 
-  //removendo o display none do input do cupom
+  //expandindo e rotacionando o toggle
 
   span.addEventListener('click', function () {
-    divDesconto.style.display = 'block';
-    msgDesconto.style.display = 'none';
-    precoDesconto.style.display = 'none';
+    const toggle = document.getElementById('toggle-icon');
+    if (divDesconto.style.display !== 'block') {
+      divDesconto.style.display = 'block';
+      msgDesconto.style.display = 'none';
+      precoDesconto.style.display = 'none';
+      toggle.style.transform = 'rotate(180deg)';
+    } else {
+      //recolhendo e rotacionando o toggle
+      divDesconto.style.display = 'none';
+      toggle.style.transform = 'rotate(0deg)';
+    }
+  });
+
+  //Aplicando evento de desconto
+  const btnAplicar = document.getElementById('aplicarBtn');
+  btnAplicar.addEventListener('click', function () {
+    const valorOriginal = 1200;
+    const valorInput = document.getElementById('discountCode').value;
+    const mensagemNovoValor = divDesconto.lastElementChild;
+
+    //verificando valor do campo de desconto e aplicando o desconto
+    if (valorInput === 'DESCONTO10') {
+      let novoValor = valorOriginal - (valorOriginal * 10) / 100;
+      msgDesconto.style.display = 'block';
+      msgDesconto.style.color = 'green';
+      mensagemNovoValor.textContent = `Preço Total: R$ ${novoValor.toFixed(2)}`;
+      mensagemNovoValor.style.display = 'block';
+    } else {
+      //verificação se o cupom for inválido
+      msgDesconto.textContent = 'Cupom Inválido';
+      msgDesconto.style.color = 'red';
+      msgDesconto.style.display = 'block';
+    }
   });
 });
